d3584809a94e1d1e2bcbe22fabcf346004d21cbd
VITOR MIGUEL RODRIGUES CEZARIO <25132565@CFP127ALUNO.LOCAL>
2025-09-22 16:55:55 -0300

    Iniciando API atividade

--- modulo/funcoes.js
+++ modulo/funcoes.js
@@ -32,6 +32,10 @@ const getEstadoBySigla = function (siglaNotTrat) {
 
     let sigla = String(siglaNotTrat).toUpperCase()
 
+    if(!isNaN(sigla)){
+        return MESSAGE_ERROR
+    }
+
     let message = { status: true, statuscode: 200, development: 'Vitor Miguel Rodrigues Cezario', uf: sigla }
 
     dados.listaDeEstados.estados.forEach(function (item) {
@@ -53,6 +57,10 @@ const getCapitalBySigla = function (siglaNotTrat) {
 
     let sigla = String(siglaNotTrat).toUpperCase()
 
+    if(!isNaN(sigla)){
+        return MESSAGE_ERROR
+    }
+
     let message = { status: true, statuscode: 200, development: 'Vitor Miguel Rodrigues Cezario', uf: sigla }
 
     dados.listaDeEstados.estados.forEach(function (item) {
@@ -69,31 +77,95 @@ const getCapitalBySigla = function (siglaNotTrat) {
 }
 
 //Retorna a lista de estados filtrando pela região
-const getEstadosByRegiao = function (regiao) {
-    let message = { status: true, statuscode: 200, development: 'Vitor Miguel Rodrigues Cezario', regiao: regiao, estados: [] }
+const getEstadosByRegiao = function (regiaoNotTrat) {
+
+    regiao = regiaoNotTrat.charAt(0).toUpperCase() + regiaoNotTrat.slice(1).toLowerCase()
+
+    if(!isNaN(regiao)){
+        return MESSAGE_ERROR
+    }
+
+    let message = { status: true, statuscode: 200, development: 'Vitor Miguel Rodrigues Cezario', regiao: regiao, estados: []}
 
     dados.listaDeEstados.estados.forEach(function (item) {
-        if (item.regiao == regiao) {
-            message.estados = item.cidades
+        if(item.regiao == regiao){
+            uf = item.sigla
+            descricao = item.nome
+            message.estados.push({'uf': uf, 'descricao': descricao}) 
         }
     })
 
+    if (message.regiao.length > 0)
+        return message //Resultado Verdadeiro da API 200
+    else
+        return MESSAGE_ERROR //Resultado Falso da API 500 
 }
 
 //Retorna a lista de estados que forma a capital de um país filtrando pelo País
-const getEstadoIsCapitalByCountry = function (pais) {
+const getEstadoIsCapitalByCountry = function (paisNotTrat) {
 
+    pais = paisNotTrat.charAt(0).toUpperCase() + paisNotTrat.slice(1).toLowerCase()
+
+    if(!isNaN(pais)){
+        return MESSAGE_ERROR
+    }
+
+    let message = { status: true, statuscode: 200, development: 'Vitor Miguel Rodrigues Cezario', capitais: []}
+
+    if(dados.listaDeEstados.pais == pais){
+        dados.listaDeEstados.estados.forEach(function (item) {
+                if(item.capital_pais){
+
+                    capital_atual = item.capital_pais.capital
+                    capital_pais_ano_inicio = item.capital_pais.ano_inicio
+                    capital_pais_ano_termino = item.capital_pais.ano_fim
+                    regiao = item.regiao
+                    uf = item.sigla
+                    descricao = item.nome
+
+                    message.capitais.push({capital_atual: capital_atual, uf: uf, descricao: descricao,
+                                            capital: item.nome, regiao: regiao, capital_pais_ano_inicio: capital_pais_ano_inicio,
+                                            capital_pais_ano_termino: capital_pais_ano_termino })
+                }
+        })
+    }
+
+    if (pais.length > 0)
+        return message //Resultado Verdadeiro da API 200
+    else
+        return MESSAGE_ERROR //Resultado Falso da API 500 
 }
 
 //Retorna as cidades existentes em um estado, filtrando pela sigla
-const getCidadesBySigla = function (sigla) {
+const getCidadesBySigla = function (siglaNotTrat) {
 
+    sigla = siglaNotTrat.toUpperCase()
+
+    if(!isNaN(sigla)){
+        return MESSAGE_ERROR
+    }
+
+    let message = { status: true, statuscode: 200, development: 'Vitor Miguel Rodrigues Cezario', uf: sigla, descricao: [], quantidade_cidades: [], cidades: []}
+
+    dados.listaDeEstados.estados.forEach(function (item) {
+        if (item.sigla == sigla) {
+            message.descricao = item.nome
+            message.quantidade_cidades = item.cidades.length
+            message.cidades = item.cidades.map(cidade => cidade.nome);
+        }
+    })
+    
+    if (sigla.length > 0 && sigla.length < 3 && sigla.length != 1)
+        return message //Resultado Verdadeiro da API 200
+    else
+        return MESSAGE_ERROR //Resultado Falso da API 500 
 }
-
-console.log(getEstadoBySigla(""))
 
 module.exports = {
     getAllEstados,
     getEstadoBySigla,
-    getCapitalBySigla
+    getCapitalBySigla,
+    getEstadosByRegiao,
+    getEstadoIsCapitalByCountry,
+    getCidadesBySigla
 }
